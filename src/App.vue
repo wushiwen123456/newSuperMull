c23ff138766e6a5891f599864ae7c8549cef15b3
wushiwen <1131634405@qq.com>
2019-10-28 05:14:06 +0800

    第二次提交

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+} 
+</script>
+
+<style>
+  /* 引入公共样式 */
+  @import "./assets/css/base.css";
+ 
+ 
+</style>
