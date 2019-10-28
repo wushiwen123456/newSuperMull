33751aec7b27c0679a0831c34d1c12311322ad94
wushiwen <1131634405@qq.com>
2019-10-29 00:45:34 +0800

    2019/10/29

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,39 @@
 <template>
-  <h2>Home</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
 export default {
-  name:"Home"
+  name:"Home",
+  data(){
+    return {
+      banners:[],
+      recommends:[],
+    }
+  },
+  components:{
+    NavBar
+  },
+  created(){
+    //1. 请求多个数据
+    getHomeMultidata().then(res => {
+      // console.log(res)
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  }
 }
 </script>
 
 <style scoped>
-
+ .home-nav{
+   background-color: var(--color-tint);
+   color: #fff;
+ }
 </style>
